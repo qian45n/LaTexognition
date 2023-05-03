8b27234f546fb8efea90225339b1682ada7c1485
qian45n <131606310+qian45n@users.noreply.github.com>
2023-05-02 21:24:29 -0400

    fixed typo in setup.sh

--- setup.sh
+++ setup.sh
@@ -3,4 +3,4 @@ pip install opencv-python-headless==4.1.2.30 -U -q
 python -m pix2tex.dataset.dataset -i dataset/data/images_train -e dataset/data/im2latex_formulas.norm.lst -o dataset/data/train.pkl
 python -m pix2tex.dataset.dataset -i dataset/data/images_val -e dataset/data/im2latex_formulas.norm.lst -o dataset/data/val.pkl
 python -m pix2tex.dataset.dataset -i dataset/data/images_test -e dataset/data/im2latex_formulas.norm.lst -o dataset/data/test.pkl
-python -m pix2tex.dataset.dataset --equations dataset/data/im2latex_formulas.norm.lst --vocab-size 1000 --out dataset/data/tokenizer.json
+python -m pix2tex.dataset.dataset --equations dataset/data/im2latex_formulas.norm.lst --vocab-size 1000 --out dataset/data/tokenizer_small.json
